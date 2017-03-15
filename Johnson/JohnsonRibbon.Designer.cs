1c405c56665955abeba22032dca625062b34515a
Brian Gibson <bggibson@hotmail.com>
2017-03-15 12:40:09 -0400

    Add Ribbon

---
+++ Johnson/JohnsonRibbon.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Johnson
+{
+    partial class JohnsonRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public JohnsonRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.btnInitial = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "JOHNSON";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.btnInitial);
+            this.group1.Label = "Fit";
+            this.group1.Name = "group1";
+            // 
+            // btnInitial
+            // 
+            this.btnInitial.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnInitial.Label = "Initial";
+            this.btnInitial.Name = "btnInitial";
+            this.btnInitial.OfficeImageId = "HappyFace";
+            this.btnInitial.ShowImage = true;
+            // 
+            // JohnsonRibbon
+            // 
+            this.Name = "JohnsonRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.JohnsonRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnInitial;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal JohnsonRibbon JohnsonRibbon
+        {
+            get { return this.GetRibbon<JohnsonRibbon>(); }
+        }
+    }
+}
---
+++ Johnson/JohnsonRibbon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace Johnson
+{
+    public partial class JohnsonRibbon
+    {
+        private void JohnsonRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
