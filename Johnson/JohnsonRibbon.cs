d313f65acd675c46f4579c49bb4b744fd675a3b1
Brian Gibson <bggibson@hotmail.com>
2017-03-15 18:13:05 -0400

    Delete RangeSelectorForm Add InputBox functionality to JohnsonRibbon

--- Johnson/JohnsonRibbon.cs
+++ Johnson/JohnsonRibbon.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Office.Tools.Ribbon;
+﻿using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Johnson
 {
@@ -15,7 +12,13 @@ namespace Johnson
 
         private void OnInitalButtonClick(object sender, RibbonControlEventArgs e)
         {
-        
+            string Prompt = "Select two data columns (X, Freq) for analysis";
+            string Title = "Select Histogram";
+            dynamic range = Globals.ThisAddIn.Application.InputBox(Prompt: Prompt,Title: Title, Type: 8);
+            if(range is Excel.Range)
+            {
+                ;
+            }
         }
     }
 }
